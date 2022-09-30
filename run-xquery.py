dbab6f4e8dead4a34d649f4daf35e95133876fb2
David McKee <dragon@dxw.com>
2022-09-30 15:43:11 +0100

    Factor out variables

--- run-xquery.py
+++ run-xquery.py
@@ -2,9 +2,15 @@ import environ
 environ.Env.read_env()
 import caselawclient.Client
 from requests_toolbelt.multipart import decoder
+import json
 client = caselawclient.Client.api_client
 
-response = client.eval(xquery_path="my_xquery.xqy", vars='{"mindoc":1, "maxdoc":4}', accept_header="application/xml")
+mindoc=1
+maxdoc=3
+vars = json.dumps({'mindoc': mindoc, 'maxdoc': maxdoc})
+
+
+response = client.eval(xquery_path="my_xquery.xqy", vars=vars, accept_header="application/xml")
 multipart_data = decoder.MultipartDecoder.from_response(response)
 for part in multipart_data.parts:
   print (part.text)
